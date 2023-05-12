6f9d13016d00eb1e24b5de694d99df797f1d9050
Antonio Hickey <antoniohickey99@gmail.com>
2023-05-12 11:53:26 -0400

    basic ip & tcp packet parsing

--- networking/TCP/tcp_implementation/run.sh
+++ networking/TCP/tcp_implementation/run.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cargo b --release
 sudo setcap cap_net_admin=eip target/release/tcp_implementation
 ./target/release/tcp_implementation &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
--- networking/TCP/tcp_implementation/src/main.rs
+++ networking/TCP/tcp_implementation/src/main.rs
@@ -1,15 +1,55 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-
     // Create tun tap interface for spoofing kernel's NIC to steal TCP packets
     // into user space.
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
 
     // Packet buffer
-    let mut buf = vec![0u8; 1504]; // MTU + 4 for the header
-    let nbytes = nic.recv(&mut buf[..])?;
-    println!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    let mut buf = [0u8; 1504]; // MTU + 4 for the header
+    loop {
+        // parsing packet buffer
+        let nbytes = nic.recv(&mut buf[..])?;
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // no ipv4
+            continue;
+        }
+            
+        // Parsing the ip packet
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dest = p.destination_addr();
+                let proto = p.protocol();
+                if proto != 0x06 {
+                    //not tcp
+                    continue;
+                }
 
+                // Parsing the TCP packet
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4+p.slice().len()..]) {
+                    Ok(p) => {
+                        println!(
+                            "{} -> {} | {} bytes | port: {}", 
+                            src, 
+                            dest, 
+                            p.slice().len(), 
+                            p.destination_port(),
+                        );
+                    }, 
+                    Err(e) => {
+                        println!("ignoring weird tcp packet {:?}", e);
+                    }
+                }
+
+                println!("{} -> {} | {} bytes | protocol: {}", src, dest, p.payload_len(), proto);
+            },
+            Err(e) => {
+                println!("ignoring weird packet {:?}", e);
+            }
+        }
+    }
     Ok(())
 }
